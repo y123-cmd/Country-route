c09e09c12420f7afb5003d678fd9d74c669066ee
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-12-08 16:49:26 +0300

    updated my country model by adding ignore properties to ignore uncessesary fields

--- src/main/java/com/example/routing/model/Country.java
+++ src/main/java/com/example/routing/model/Country.java
@@ -1,7 +1,9 @@
 package com.example.routing.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Country {
     private String cca3;
     private List<String> borders;
